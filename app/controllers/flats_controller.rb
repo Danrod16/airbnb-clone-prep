f671fb9c4fc47223759cb9157d3593573a310f28
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-11-15 17:11:35 +0100

    Fix: added some views

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,6 +4,12 @@ class FlatsController < ApplicationController
   # GET /flats
   def index
     @flats = Flat.all
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
   end
 
   # GET /flats/1
